e868daf6a4c73bde88446e90964d9fc3bf7082ea
Fredrick-Onyango <fredrickonyango370@gmail.com>
2020-01-29 10:12:28 +0300

    Second Commit- BanK Transaction Automation

--- src/testcases/CustomerLogin.java
+++ src/testcases/CustomerLogin.java
@@ -18,10 +18,10 @@ import org.testng.annotations.Test;
  *
  */
 public class CustomerLogin {
-		    //set Driver & open url
-			WebDriver driver;
-			@Test(priority = 1)
-			public void OpenBrowser() throws InterruptedException {
+    //set Driver & open url
+	WebDriver driver;
+	@Test(priority = 1)
+	public void OpenBrowser() throws InterruptedException {
 				try {
 		     System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"//lib//chromedriver");
 		     driver = new ChromeDriver();
@@ -39,27 +39,28 @@ public class CustomerLogin {
 				// TODO Auto-generated method stub
 				
 			}
-            //Click Customer
-			@Test(priority = 2)
-			public void VerifyClickCustomer() {
+    //Click Customer
+	@Test(priority = 2)
+	public void VerifyClickCustomer() {
 				new WebDriverWait(driver, 100).until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[3]/div/div[2]/div/div[1]/div[1]/button"))).click();
 			}
-			//Select Customer from Drop-down
-			@Test(priority = 3)
-			public void VerifySelectCustomer() {
-				new WebDriverWait(driver, 100).until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"userSelect\"]")));
-			    Select customer = new Select(driver.findElement(By.id("userSelect")));
-				customer.selectByVisibleText("Hermoine Granger");
-				customer.selectByIndex(1);
+	//Select Customer from Drop-down
+    @Test(priority = 3)
+    public void VerifySelectCustomer() {
+			new WebDriverWait(driver, 100).until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"userSelect\"]")));
+			Select customer = new Select(driver.findElement(By.id("userSelect")));
+			customer.selectByVisibleText("Hermoine Granger");
+			customer.selectByIndex(1);
 			}
-			
-			@Test(priority = 4)
-			public void VerifyLogin() {
+	//Verify Login		
+	@Test(priority = 4)
+	public void VerifyLogin() {
 				new WebDriverWait(driver, 20).until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[3]/div/div[2]/div/form/button"))).click();
 			}
-			
-			@Test(priority = 5)
-			public void VerifyCustomerView() {
+	/*
+	//Assert Customer details		
+	@Test(priority = 5)
+	public void VerifyCustomerView() {
 			//Assert Customer name
 			//WebElement name = new WebDriverWait(driver, 20).until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[1]/div/div[2]/div/div[1]/strong/span")));
 			WebElement name =driver.findElement(By.cssSelector("body > div > div > div.ng-scope > div > div:nth-child(1) > strong > span"));
@@ -101,63 +102,66 @@ public class CustomerLogin {
 			}else{
 			System.out.println("Expected text is not obtained");
 			}
-	}
-			@Test(priority = 6)
-			public void VerifySuccessfulWithdraw() {
-				//Click withdraw button
-				new WebDriverWait(driver, 20).until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[3]/div/div[2]/div/div[3]/button[3]"))).click();
-				//Withdraw 1000
-				new WebDriverWait(driver, 20).until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[3]/div/div[2]/div/div[4]/div/form/div/input"))).sendKeys("1000");
-				//Click submit withdrawal button
-				new WebDriverWait(driver, 20).until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[3]/div/div[2]/div/div[4]/div/form/button"))).click();
-				String bodyText = driver.findElement(By.xpath("/html/body/div[3]/div/div[2]/div/div[4]/div/span")).getText();
-				CharSequence text = "Transaction successful";
-				Assert.assertTrue(bodyText.contains(text));
-				System.out.println("Test Passed");
+	        }
+	      */  
+	  //Successful Withdrawal transaction
+	  @Test(priority = 6)
+	  public void VerifySuccessfulWithdraw() {
+			//Click withdraw button
+			new WebDriverWait(driver, 20).until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[3]/div/div[2]/div/div[3]/button[3]"))).click();
+			//Withdraw 1000
+			new WebDriverWait(driver, 20).until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[3]/div/div[2]/div/div[4]/div/form/div/input"))).sendKeys("1000");
+			//Click submit withdrawal button
+			new WebDriverWait(driver, 20).until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[3]/div/div[2]/div/div[4]/div/form/button"))).click();
+			String bodyText = driver.findElement(By.xpath("/html/body/div[3]/div/div[2]/div/div[4]/div/span")).getText();
+			CharSequence text = "Transaction successful";
+			Assert.assertTrue(bodyText.contains(text));
+			System.out.println("Test Passed");
+			WebElement element=driver.findElement(By.xpath("/html/body/div[3]/div/div[2]/div/div[4]/div/span"));
 				
-				
-				WebElement element=driver.findElement(By.xpath("/html/body/div[3]/div/div[2]/div/div[4]/div/span"));
-				//To store the text of a WebElement in a string
-				String text1=element.getText();
-				//For printing the text in console output
-				System.out.println("Text obtained is "+text1);
-				if(text1.contains("Expected text"))
+			//To store the text of a WebElement in a string
+			String text1=element.getText();
+			//For printing the text in console output
+			System.out.println("Text obtained is "+text1);
+			if(text1.contains("Expected text"))
 						{
-				System.out.println("Expected text is obtained");
-				}else{
-				System.out.println("Expected text is not obtained");
-				}
-				driver.navigate().refresh();
-			}		
-			@Test(priority = 7)
-			public void VerifyFailedWithdraw() {
-				//Click withdraw button
-				new WebDriverWait(driver, 20).until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[3]/div/div[2]/div/div[3]/button[3]"))).click();
-				//Withdraw 1000
-				new WebDriverWait(driver, 20).until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[3]/div/div[2]/div/div[4]/div/form/div/input"))).sendKeys("10000");
-				//Click submit withdrawal button
-				new WebDriverWait(driver, 20).until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[3]/div/div[2]/div/div[4]/div/form/button"))).click();
-				String bodyText = driver.findElement(By.xpath("/html/body/div[3]/div/div[2]/div/div[4]/div/span")).getText();
-				CharSequence text = "Transaction Failed. You can not withdraw amount more than the balance.";
-				Assert.assertTrue(bodyText.contains(text));
-				System.out.println("Test Passed");
-				
-				
-				WebElement element=driver.findElement(By.xpath("/html/body/div[3]/div/div[2]/div/div[4]/div/span"));
-				//To store the text of a WebElement in a string
-				String text1=element.getText();
-				//For printing the text in console output
-				System.out.println("Text obtained is "+text1);
-				if(text1.contains("Expected text"))
-						{
-				System.out.println("Expected text is obtained");
-				}else{
-				System.out.println("Expected text is not obtained");
-				}
-				driver.navigate().refresh();
+			System.out.println("Expected text is obtained");
+			}else{
+			System.out.println("Expected text is not obtained");
+			}
+			driver.navigate().refresh();
 			}	
-            @Test(priority = 8)
-	        public void CloseBrowser() {
+	//Failed Withdrawal Transaction
+	@Test(priority = 7)
+	public void VerifyFailedWithdraw() {
+			//Click withdraw button
+			new WebDriverWait(driver, 20).until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[3]/div/div[2]/div/div[3]/button[3]"))).click();
+			//Withdraw 1000
+			new WebDriverWait(driver, 20).until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[3]/div/div[2]/div/div[4]/div/form/div/input"))).sendKeys("10000");
+			//Click submit withdrawal button
+			new WebDriverWait(driver, 20).until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[3]/div/div[2]/div/div[4]/div/form/button"))).click();
+			String bodyText = driver.findElement(By.xpath("/html/body/div[3]/div/div[2]/div/div[4]/div/span")).getText();
+			CharSequence text = "Transaction Failed. You can not withdraw amount more than the balance.";
+			Assert.assertTrue(bodyText.contains(text));
+			System.out.println("Test Passed");
+					
+				
+			WebElement element=driver.findElement(By.xpath("/html/body/div[3]/div/div[2]/div/div[4]/div/span"));
+			//To store the text of a WebElement in a string
+			String text1=element.getText();
+			//For printing the text in console output
+			System.out.println("Text obtained is "+text1);
+			if(text1.contains("Expected text"))
+					{
+			System.out.println("Expected text is obtained");
+			}else{
+			System.out.println("Expected text is not obtained");
+			}
+			driver.navigate().refresh();
+			}
+	//Close Browser
+    @Test(priority = 8)
+    public void CloseBrowser() {
 		    driver.close();
 		    }
 				
